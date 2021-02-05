f49647e3004c78ea6aa5de31f5f6867bdfde7732
Johnatan <jf.johnatan@gmail.com>
2021-02-05 11:43:47 +0700

    create basic home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    
+  <Jumbotron>
+    <h1>Welcome!</h1>
+    <p>
+      Description text goes here.
+    </p>
+      
+    <Link to='/login'>
+      <Button variant="primary">Login</Button>
+    </Link>
+    <Link to='/signup'>
+      <Button variant="primary">Sign Up</Button>
+    </Link>
+  </Jumbotron>    
   )
 }
 
