16fd73d614dc037598a79eec3fd04fe7af8f9ac9
Johnatan <jf.johnatan@gmail.com>
2021-02-23 09:07:19 +0700

    change baseUrl according to environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,9 @@
 let baseUrl = 'https://book-app-backend-5.herokuapp.com';
 
+if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  baseUrl = 'http://localhost:3001';
+}
+
 export default {
   baseUrl,
 }
