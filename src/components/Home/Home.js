0f8f2fbc8aff815ec47f8efb59da9726aaa454dc
Johnatan <jf.johnatan@gmail.com>
2021-03-31 14:14:43 +0700

    redesign homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h1>BookApp</h1>
-      <h2>Organise your reading list in one place.</h2>
-      <div>
-        <Link to='/login'>
-          <button>Login</button>
-        </Link>
-        <span>or</span>
-        <Link to='/signup'>
-          <button>Sign up</button>
-        </Link>
+    <div className="home">
+      <div className="home__card">
+        <h1 className="home__heading">BookApp</h1>
+        <h2 className="home__description">Organise your reading list in one place.</h2>
+        <hr className="home__horizontal-separator" />
+        <div className="home__button-container">
+          <Link to='/login'>
+            <button className="home__button home__button--login">Login</button>
+          </Link>
+          <span className="home__description">or</span>
+          <Link to='/signup'>
+            <button className="home__button home__button--sign-up">Sign up</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
