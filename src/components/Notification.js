fe7b7c5e53d1215f37fe88cb6f5b528a7e23c01c
Johnatan <jf.johnatan@gmail.com>
2021-03-31 12:30:23 +0700

    add logic to notification bar

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,8 +4,8 @@ import IcomoonReact, { iconList } from "icomoon-react";
 
 const Notification = props => {
   return (
-    <div className="notification notification--info">
-      <IcomoonReact className="notification__icon" iconSet={iconSet} icon="books" />
+    <div className={`notification ${props.success ? 'notification--success' : } ${!props.notification.message ? 'notification--invisible' : ''}`}>
+      <IcomoonReact className="notification__icon" iconSet={iconSet} icon={`${props.notification.success ? 'checkmark' : 'warning'}`} />
       <p className="notification__message">{ props.notification.message ? props.notification.message : '&nbsp;' }</p>
     </div>
   )
