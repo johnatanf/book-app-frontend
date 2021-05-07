f8bc88379984a9056fbbbdf175f8802d9c563c0f
Johnatan Fallen <johnatan.f100@gmail.com>
2021-05-07 09:49:18 +0700

    truncate subtitle if it's longer than 50 characters

--- src/components/Books/BookGrid.js
+++ src/components/Books/BookGrid.js
@@ -38,13 +38,15 @@ const BookGrid = ({ book, query, flashNotification, setSearchedBooks }) => {
       flashNotification('Failed to view book', 'failure');
     }
   }
+
+  const truncateSubtitle = (subtitle = '') => subtitle.length > 50 ? `${subtitle.slice(0, 50)}...` : subtitle
   
   return (
     <div className="book-grid">
       <div>
         <img className="book-grid__img" src={book && book.bookCoverUrl ? book.bookCoverUrl : notAvailableImage} alt={`${book.title} book cover`}/>
         <h2 className="book-grid__title">{book ? book.title : ''}</h2>
-        <h3 className="book-grid__subtitle">{book ? book.subtitle : ''}</h3>
+        <h3 className="book-grid__subtitle">{book ? truncateSubtitle(book.subtitle) : ''}</h3>
         <h3 className="book-grid__author">{book.authors ? book.authors.join(', ') : ''}</h3>
       </div>
       { book._id && match.path === '/search' ? <h3 className="mb-3 text-muted">Already in reading list</h3> : null }
